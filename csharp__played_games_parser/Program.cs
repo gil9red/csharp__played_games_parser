b7c9e09bafc86e64d7b12246987e1bbfc96fdcc3
ipetrash <ilya.petrash@inbox.ru>
2017-06-19 12:42:41 +0500

    Initial commit

---
+++ csharp__played_games_parser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp__played_games_parser
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start!");
+        }
+    }
+}
