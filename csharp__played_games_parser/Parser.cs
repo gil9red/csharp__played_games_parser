f71da1f0324485a512c7d7029a3d0d88007e97d3
ipetrash <ilya.petrash@inbox.ru>
2017-06-20 16:46:19 +0500

    The parser is complete

--- csharp__played_games_parser/Parser.cs
+++ csharp__played_games_parser/Parser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace csharp__played_games_parser
 {
@@ -14,7 +15,8 @@ namespace csharp__played_games_parser
         public const string NOT_FINISHED_WATCHED = "NOT_FINISHED_WATCHED";
 
         /// Регулярка вытаскивает выражения вида: 1, 2, 3 или 1-3, или римские цифры: III, IV
-        //private const Pattern PARSE_GAME_NAME_PATTERN = Pattern.compile("(\\d+(, *?\\d+)+)|(\\d+ *?- *?\\d+)|([MDCLXVI]+(, ?[MDCLXVI]+)+)", Pattern.CASE_INSENSITIVE);
+//        private const Pattern PARSE_GAME_NAME_PATTERN = Pattern.compile("(\\d+(, *?\\d+)+)|(\\d+ *?- *?\\d+)|([MDCLXVI]+(, ?[MDCLXVI]+)+)", Pattern.CASE_INSENSITIVE);
+        private static Regex PARSE_GAME_NAME_PATTERN = new Regex(@"(\d+(, *?\d+)+)|(\d+ *?- *?\d+)|([MDCLXVI]+(, ?[MDCLXVI]+)+)", RegexOptions.Compiled);
         
         /// <summary>
         /// Функция парсит переданный текст с списком игр.
@@ -28,26 +30,28 @@ namespace csharp__played_games_parser
             var platforms = new Dictionary<string, IDictionary<string, List<string>>>();
             IDictionary<string, List<string>> platform = null;
 
-            for (var i = 0; i < lines.Length; i++)
+            foreach (var l in lines)
             {
-                var line = lines[i].TrimEnd();
+                var line = l.TrimEnd();
                 if (string.IsNullOrEmpty(line))
                 {
                     continue;
                 }
 
-                bool hasFlag1 = " -@".Contains(line[0].ToString());
-                bool hasFlag2 = " -@".Contains(line[1].ToString());
+                var hasFlag1 = " -@".Contains(line[0].ToString());
+                var hasFlag2 = " -@".Contains(line[1].ToString());
 
                 if (!hasFlag1 && !hasFlag2 && line.EndsWith(":"))
                 {
                     var platformName = line.Substring(0, line.Length - 1);
 
-                    platform = new Dictionary<string, List<string>>();
-                    platform[FINISHED_GAME] = new List<string>();
-                    platform[NOT_FINISHED_GAME] = new List<string>();
-                    platform[FINISHED_WATCHED] = new List<string>();
-                    platform[NOT_FINISHED_WATCHED] = new List<string>();
+                    platform = new Dictionary<string, List<string>>
+                    {
+                        [FINISHED_GAME] = new List<string>(),
+                        [NOT_FINISHED_GAME] = new List<string>(),
+                        [FINISHED_WATCHED] = new List<string>(),
+                        [NOT_FINISHED_WATCHED] = new List<string>()
+                    };
 
                     platforms[platformName] = platform;
 
@@ -63,30 +67,33 @@ namespace csharp__played_games_parser
 
                 string categoryName = null;
 
-                if (flag.Equals("  "))
+                switch (flag)
                 {
-                    categoryName = FINISHED_GAME;
-                }
-                else if (flag.Equals(" -") || flag.Equals("- "))
-                {
-                    categoryName = NOT_FINISHED_GAME;
-                }
-                else if (flag.Equals(" @") || flag.Equals("@ "))
-                {
-                    categoryName = FINISHED_WATCHED;
-                }
-                else if (flag.Equals("@-") || flag.Equals("-@"))
-                {
-                    categoryName = NOT_FINISHED_WATCHED;
+                    case "  ":
+                        categoryName = FINISHED_GAME;
+                        break;
+                        
+                    case " -":
+                    case "- ":
+                        categoryName = NOT_FINISHED_GAME;
+                        break;
+                        
+                    case " @":
+                    case "@ ":
+                        categoryName = FINISHED_WATCHED;
+                        break;
+                        
+                    case "@-":
+                    case "-@":
+                        categoryName = NOT_FINISHED_WATCHED;
+                        break;
+                        
+                    default:
+                        Console.WriteLine("Invalid line format: \"{0}\"", line);
+                        continue;
                 }
 
-                if (string.IsNullOrEmpty(categoryName))
-                {
-                    Console.WriteLine("Invalid line format: \"{0}\"", line);
-                    continue;
-                }
-
-                List<string> category = platform[categoryName];
+                var category = platform[categoryName];
 
                 var gameName = line.Substring(2);
                 foreach (var game in ParseGameName(gameName))
@@ -118,12 +125,55 @@ namespace csharp__played_games_parser
         /// </example>
         /// <param name="gameName">Название игры</param>
         /// <returns>Список названий игр.</returns>
-        public static List<string> ParseGameName(string gameName)
+        private static List<string> ParseGameName(string gameName)
         {
-            var games = new List<string>();
-            games.Add(gameName);
+            var match = PARSE_GAME_NAME_PATTERN.Match(gameName);
+            if (!match.Success)
+            {
+                return new List<string>() { gameName };
+            }
 
-            // TODO: complete this
+            var seqStr = match.Value;
+            
+            // "Resident Evil 4,  5,   6" -> "Resident Evil"
+            // For not valid "Trollface Quest 1-7-8" -> "Trollface Quest"
+            var index = gameName.IndexOf(seqStr);
+            var baseName = gameName.Substring(0, index).Trim();
+
+            // "4,  5,   6" -> "4,5,6"
+            seqStr = seqStr.Replace(" ", "");
+            
+            var seq = new List<string>();
+
+            if (seqStr.Contains(",")) {
+                seq.AddRange(seqStr.Split(','));
+
+            } else if (seqStr.Contains("-")) {
+                // "1-7" -> ["1", "7"] -> ["1", "2", "3", "4", "5", "6", "7"]
+                var parts = seqStr.Split('-');
+
+                var a = int.Parse(parts[0]);
+                var b = int.Parse(parts[1]);
+
+                for (; a <= b; a++) {
+                    seq.Add(a.ToString());
+                }
+
+            } else {
+                return new List<string>() { gameName };
+            }
+            
+            
+            var games = new List<string>();
+            
+            foreach (var num in seq) {
+                if (num == "1") {
+                    games.Add(baseName);
+                } else {
+                    // Example: "Resident Evil" + " " + "4"
+                    games.Add(baseName + " " + num);
+                }
+            }
 
             return games;
         }
--- csharp__played_games_parser/Program.cs
+++ csharp__played_games_parser/Program.cs
@@ -8,28 +8,29 @@ namespace csharp__played_games_parser
     internal class Program
     {
         public static void Main(string[] args)
-        {
-//            * "Resident Evil 4, 5, 6" -> ["Resident Evil 4", "Resident Evil 5", "Resident Evil 6"]
-//            * "Resident Evil 1-3"     -> ["Resident Evil", "Resident Evil 2", "Resident Evil 3"]
-//            * "Resident Evil 4"       -> ["Resident Evil 4"]
-            Console.WriteLine(string.Join("\t", Parser.ParseGameName("Resident Evil 4, 5, 6").ToArray()));
-            Console.WriteLine(string.Join("\t", Parser.ParseGameName("Resident Evil 4,   5,    6").ToArray()));
-            Console.WriteLine(string.Join("\t", Parser.ParseGameName("Resident Evil 1-3").ToArray()));
-            Console.WriteLine(string.Join("\t", Parser.ParseGameName("Resident Evil 1 -  3").ToArray()));
-            Console.WriteLine(string.Join("\t", Parser.ParseGameName("Resident Evil 4").ToArray()));
-         
+        {   
+            var readText = File.ReadAllText("../../../2017-06-08.txt");
             
-//            var readText = File.ReadAllText("../../../2017-06-08.txt");
-////            Console.WriteLine(readText);
-//            
-//            Dictionary<string, IDictionary<string, List<string>>> platforms = Parser.Parse(readText);
-//            Console.WriteLine("Platforms: " + platforms.Count);
-//                
-//            var totalGames = platforms.Values.SelectMany(categories => categories.Values).Sum(games => games.Count);
-//            Console.WriteLine("Games: " + totalGames);
-//            Console.WriteLine();
-//            Console.WriteLine(string.Join(", ", platforms.Keys.ToArray()));
-////            Console.WriteLine(platforms);
+            var platforms = Parser.Parse(readText);
+            Console.WriteLine("Platforms: " + platforms.Count);
+                
+            var totalGames = platforms.Values.SelectMany(categories => categories.Values).Sum(games => games.Count);
+            Console.WriteLine("Games: " + totalGames);
+            Console.WriteLine();
+            Console.WriteLine(string.Join(", ", platforms.Keys.ToArray()));
+            Console.WriteLine();
+            Console.WriteLine("PS 2 / " + Parser.FINISHED_GAME);
+            foreach (var game in platforms["PS 2"][Parser.FINISHED_GAME])
+            {
+                Console.WriteLine(game);
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Sega / " + Parser.FINISHED_GAME);
+            foreach (var game in platforms["Sega"][Parser.FINISHED_GAME])
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
